abedaeb642b3569f584aa037bcb83b21a324653d
Sven <55207968+suain@users.noreply.github.com>
2021-01-06 14:46:03 +0100

    Update mobile-config.js

--- mobile-config.js
+++ mobile-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	ci: {
 		collect: {
-		        url: ['https://www.svenbelz.com', 'http://digitalnotiz.de'],
+		        url: ['https://www.svenbelz.com', 'https://www.digitalnotiz.de'],
 			numberOfRuns: 3,
 			settings: {
 				chromeFlags: '--no-sandbox',
